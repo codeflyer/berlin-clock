ac1346415895c134f541924172c32ebf5b10fe3e
Tamer Aydin <tamer.aydin@gmail.com>
2016-04-15 16:38:01 +0200

    actually use colorChangeIndex

--- client/boxes.js
+++ client/boxes.js
@@ -11,7 +11,7 @@ export class Boxes extends React.Component {
       boxes.push(
           (i < this.props.splitIndex) ?
             <div className={'box first-row row-of-' + this.props.splitIndex} style={{background: i < fiveBoxCount ?
-              (this.props.colorChangeIndex && i % 3 !== 2 ? 'yellow' : 'red') : 'darkgrey'}}></div> :
+              (this.props.colorChangeIndex && ((i + 1) % this.props.colorChangeIndex) !== 0 ? 'yellow' : 'red') : 'darkgrey'}}></div> :
             <div className={i - this.props.splitIndex === 0 ? 'box second-row first' : 'box second-row'} style={{background: (i - this.props.splitIndex) < oneBoxCount ? (this.props.colorChangeIndex ? 'yellow' : 'red') : 'darkgrey'}}></div>
           );
     }
